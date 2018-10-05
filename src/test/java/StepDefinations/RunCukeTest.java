85f1f40e34ffa2745f7327b775420294a46cbdf4
Anshul1111 <43767860+Anshul1111@users.noreply.github.com>
2018-10-05 13:04:11 +0530

    Update RunCukeTest.java

--- src/test/java/StepDefinations/RunCukeTest.java
+++ src/test/java/StepDefinations/RunCukeTest.java
@@ -3,7 +3,7 @@ package StepDefinations;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-//gjfjbgjbgjfbgjfbgj
+//Hi Anshul
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/java/features",
